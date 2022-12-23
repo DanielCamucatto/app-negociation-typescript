eac69213c5324fa98cd3be127cdefe3a428d0322
Daniel Camucatto <dcamucatto@dw3.office>
2022-12-23 19:08:50 -0300

    first add

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+import { Negociation } from './models/negociation.js'
+
+const negociation = new Negociation(new Date(), 10, 100);
+
+console.log(negociation);
+negociation.amount = 1000;
+console.log(negociation);
---
+++ dist/js/models/negociation.js
@@ -0,0 +1,11 @@
+export class Negociation {
+    #data; 
+    #amount;
+    #value;
+    
+    constructor(data, amount, value){
+        this.#data = data;
+        this.#amount = amount;
+        this.#value = value;
+    }
+}
