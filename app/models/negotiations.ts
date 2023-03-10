43f3d0cd1c9c13bd216248beead14b032df1c2d0
Daniel <dcamucatto@dw3tecnologia.com.br>
2023-03-10 08:45:24 -0300

    criando metodos adiciona e renderizaLista

--- app/models/negotiations.ts
+++ app/models/negotiations.ts
@@ -1,5 +1,14 @@
 import { Negociation } from "./negociation.js"
 
 export class Negociations {
-    private negociations:Array<Negociation> = []
+    private negociations:Array<Negociation> = [] // ARRAY DE NEGOCIAÇÃO COM GENERICS NEGOCIAÇAO
+    
+    // METODOS PARA A NEGOCIAÇÕES 
+    add(negociation: Negociation){ 
+    this.negociations.push(negociation); //ADICIONA UMA NEGOCIAÇÃO A LISTA DE NEGOCIAÇOES
+    }
+
+    renderList():Array<Negociation> {
+         return this.negociations; // RENDERIZA UM ARRAY DE NEGOCIAÇÕES 
+   }
 }
--- dist/js/models/negotiations.js
+++ dist/js/models/negotiations.js
@@ -1,5 +1,12 @@
 export class Negociations {
     constructor() {
-        this.negociations = [];
+        this.negociations = []; // ARRAY DE NEGOCIAÇÃO COM GENERICS NEGOCIAÇAO
+    }
+    // METODOS PARA A NEGOCIAÇÕES 
+    add(negociation) {
+        this.negociations.push(negociation); //ADICIONA UMA NEGOCIAÇÃO A LISTA DE NEGOCIAÇOES
+    }
+    renderList() {
+        return this.negociations; // RENDERIZA UM ARRAY DE NEGOCIAÇÕES 
     }
 }
